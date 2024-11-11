b9a4c27f970231caa44ae53b4287fec06437fc41
HerokuJR <dilishagimshan636@gmail.com>
2024-11-11 20:01:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PCIGiKQR#PCiw4EmmScTwP9XElhK1KN4I1ZOGjN4H63J6MtoUVY0",
-MONGODB: process.env.MONGODB || "mongodb://mongo:MfPJLADhRWNHvxuOORrlmfJHboedaADB@junction.proxy.rlwy.net:57828",
+SESSION_ID: process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:f4babd886f8d3c5548a5f4fd041f4d68",
+MONGODB: process.env.MONGODB || "mongodb://mongo:xAspwrXVpAyKRvrWGnucEnWyLTihRwsi@junction.proxy.rlwy.net:11262",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi GHOST-MD Is Online Now 💻\n*💻 Owner* - GHOST-MD\n\n*💻 Owner Number* 94741140620",
 SUDO_NB: process.env.SUDO_NB || "94741140620",
